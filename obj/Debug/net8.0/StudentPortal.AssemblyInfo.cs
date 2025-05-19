b486f2bd3e5441fa5295070dead34dc29a1b929e
KhulisoJohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-05-19 16:09:29 +0200

    studentcouse views added

--- obj/Debug/net8.0/StudentPortal.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentPortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29d3749b7658d8ad072477642fa0739a61e9bedd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeb2029a69e4e8f29764d77d57dcee1c2fa59a13")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
