a7f8b5aa6ccb35401b2daaa1804e788b33aec807
KhulisoJohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-07-29 19:56:53 +0200

    new update

--- obj/Debug/net8.0/StudentPortalSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentPortalSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortalSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9616d092ac7bf79f969e068a7a42bbf8b4b9142")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3857a9f23dc71994828b98a81741778c17efd23")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortalSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortalSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
